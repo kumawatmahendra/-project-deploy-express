f1e4c7b1688880420849a8f9a004dda2d34e1975
kumawatmahendra <kumawatmahendra7896@gmail.com>
2022-09-15 17:48:09 +0530

    project deploy

---
+++ problem-2.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express();
+const port = process.env.PORT || 3000;
+const data = require("./data.json")
+
+
+app.get('/student/studentList',(req,res)=>{
+        res.send(data)
+})
+
+app.listen(port,()=>{
+console.log("server is running");
+})
